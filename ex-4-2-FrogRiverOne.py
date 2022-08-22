e81c38daa3c3c19a8594bf5e6a51569bf2e7f812
John Mee <git.commit@johnmee.com>
2022-08-23 08:49:44 +1000

    Update 4.2 FrogRiverOne.
    Fixes #1

--- ex-4-2-FrogRiverOne.py
+++ ex-4-2-FrogRiverOne.py
@@ -1,20 +1,18 @@
 """
 Frog River One
 
-Find the earliest time when a frog can jump to the other side of a river.
+A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position
+ 0) and wants to get to the opposite bank (position X+1). Leaves fall from a tree onto the surface of the river.
 
-A small frog wants to get to the other side of a river. The frog is currently located at position 0, and wants to get
-to position X. Leaves fall from a tree onto the surface of the river.
-
-You are given a non-empty zero-indexed array A consisting of N integers representing the falling leaves. A[K]
-represents the position where one leaf falls at time K, measured in seconds.
+You are given an array A consisting of N integers representing the falling leaves. A[K] represents the position where
+one leaf falls at time K, measured in seconds.
 
 The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only
-when leaves appear at every position across the river from 1 to X. You may assume that the speed of the current in
-the river is negligibly small, i.e. the leaves do not change their positions once they fall in the river.
+when leaves appear at every position across the river from 1 to X (that is, we want to find the earliest moment when
+all the positions from 1 to X are covered by leaves). You may assume that the speed of the current in the river is
+negligibly small, i.e. the leaves do not change their positions once they fall in the river.
 
 For example, you are given integer X = 5 and array A such that:
-
   A[0] = 1
   A[1] = 3
   A[2] = 1
@@ -31,10 +29,10 @@ Write a function:
 
     def solution(X, A)
 
-that, given a non-empty zero-indexed array A consisting of N integers and integer X, returns the earliest time when the
-frog can jump to the other side of the river.
+that, given a non-empty array A consisting of N integers and integer X, returns the earliest time when the frog can jump
+to the other side of the river.
 
-If the frog is never able to jump to the other side of the river, the function should return -1.
+If the frog is never able to jump to the other side of the river, the function should return −1.
 
 For example, given X = 5 and array A such that:
   A[0] = 1
@@ -48,42 +46,39 @@ For example, given X = 5 and array A such that:
 
 the function should return 6, as explained above.
 
-Assume that:
+Write an efficient algorithm for the following assumptions:
 
         N and X are integers within the range [1..100,000];
         each element of array A is an integer within the range [1..X].
 
-Complexity:
-
-        expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(X), beyond input storage (not counting the storage required for input arguments).
-
-
-Elements of input arrays can be modified.
+Copyright 2009–2022 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure
+prohibited.
 """
 import unittest
 import random
 
-N_RANGE = (1, 100000)
-
 
 def solution(X, A):
     """
-    :param X: an integer. the frogs destination
-    :param A: non-empty list of integers
-    :return: an integer - the earliest time, or -1
+    :param X: [int] The frog's destination.
+    :param A: [int] A sequence of leaf arrivals at these positions.
+    :returns: [int]  The earliest time the frog can get to position x, or -1 if never.
     """
-    # count unique leaf positions and stop when there are enough
-    leaves = {}
-    for second in range(0, len(A)):
-        leaves[A[second]] = True
-        if len(leaves) == X:
-            return second
+    # The collected leaves go into a python 'set'; which ignores duplicates.
+    leaves = set()
+    # For every leaf,
+    for count, leaf in enumerate(A):
+        # if the leaf is at/before the destination,
+        if leaf <= X:
+            # add the numbered leaf to the set of leaves.
+            leaves.add(leaf)
+            # Stop when the set has enough leaves.
+            if len(leaves) >= X:    # if the len function is too slow, replace it with a counter.
+                return count
     return -1
 
 
 class TestExercise(unittest.TestCase):
-
     def test_example1(self):
         self.assertEqual(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]), 6)
 
@@ -91,7 +86,7 @@ class TestExercise(unittest.TestCase):
         self.assertEqual(solution(1, [1]), 0)
         self.assertEqual(solution(0, [2]), -1)
         self.assertEqual(solution(2, [2]), -1)
-        self.assertEqual(solution(2, [2,1]), 1)
+        self.assertEqual(solution(2, [2, 1]), 1)
         self.assertEqual(solution(2, [1, 2]), 1)
         self.assertEqual(solution(2, [1, 1]), -1)
         self.assertEqual(solution(2, [2, 2]), -1)
@@ -107,22 +102,29 @@ class TestExercise(unittest.TestCase):
 
     def test_extreme_max(self):
         X = 100000
-        A = range(1, X)
+        A = list(range(1, X))
         random.shuffle(A)
         A.append(X)
-        self.assertEqual(solution(X, A), X-1)
+        self.assertEqual(solution(X, A), X - 1)
+
+    def test_issue_1(self):
+        """Someone actually bothered to report a mistake.
+
+        https://github.com/johnmee/codility/issues/1
+        """
+        self.assertEqual(-1, solution(3, [3, 3, 1, 1, 4]))
+
+    # Codility Tests
+    # extreme_frog - frog never crosses
+    # small_random1 - 3 random permutation X=50
+    # small_random2 - 5 random permutation X=60
+    # extreme_leaves = all leaves in the same place
+    # medium_random = 6 and 2 random permutations, X = ~5000
+    # medium_range = arithmetic sequences X = 5000
+    # large_random = 10 and 100 random permutation, X = ~10000
+    # large_permutation = permutation tests (2)
+    # large_range = arithmetic sequences X=30000
 
 
-# extreme_frog - frog never crosses
-# small_random1 - 3 random permutation X=50
-# small_random2 - 5 random permutation X=60
-# extreme_leaves = all leaves in the same place
-# medium_random = 6 and 2 random permutations, X = ~5000
-# medium_range = arithmetic sequences X = 5000
-# large_random = 10 and 100 random permutation, X = ~10000
-# large_permutation = permutation tests (2)
-# large_range = arithmetic sequences X=30000
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

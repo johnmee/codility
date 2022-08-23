44b8b15b538d5a499ab4859e7560ff73e3300ca9
John Mee <git.commit@johnmee.com>
2022-08-23 22:19:22 +1000

    Update 1.1 BinaryGap.

--- ex-1-1-binarygap.py
+++ ex-1-1-binarygap.py
@@ -1,64 +1,72 @@
-import unittest
+"""
+# Binary Gap
 
-# the largest integer we have to deal with
-MAXINT = 2147483647
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at
+both ends in the binary representation of N.
+
+For example,
+The number 9 has binary representation 1001 and contains a binary gap of length 2.
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
+The number 20 has binary representation 10100 and contains one binary gap of length 1.
+The number 15 has binary representation 1111 and has no binary gaps.
+The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+    def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary gap.
+The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its
+ longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation
+ '100000' and thus no binary gaps.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..2,147,483,647].
+
+Copyright 2009–2022 by Codility Limited. All Rights Reserved.
+Unauthorized copying, publication or disclosure prohibited.
+"""
+import unittest
 
 
 def solution(N):
-    """
-    Determines the maximal 'binary gap' in an integer
-    :param N: a positive integer (between 1 and maxint or 2million odd)
-    :return: a count of the longest sequence of zeros in the binary representation of the integer
-    """
-    # protect against crazy inputs
-    if not isinstance(N, int):
-        raise TypeError("Input must be an integer")
-    if N < 1:
-        raise ValueError("Input must be a positive integer")
-    if N > MAXINT:
-        raise ValueError("Input must be a positive integer less than 2,147,483,647")
+    """Determines the maximal 'binary gap' in an integer.
 
-    # convert the number to a string containing '0' and '1' chars
+    :param N: [int] A positive integer (between 1 and maxint).
+    :return: [int] The length of the longest sequence of zeros in the binary representation of the integer.
+
+    Convert the int to a string of 0/1 chars.
+    Loop through the chars in the string.
+    When we see a zero, start the gap counter.
+    When we see a one, compare with the biggest gap, and save the bigger; reset the gap counter.
+    When we run out of characters, return the biggest gap.
+    """
+    # Convert the number to a string containing '0' and '1' chars.
     binary_string = str(bin(N))[2:]
 
-    # the longest binary gap: use None to indicate no 'gap' yet found (set to zero at the first flip)
-    max_count = None
-    # count the bits in the sequence
-    this_count = 0
-    # true if the last bit was a zero
-    was_zero = None
-
-    # loop over all the 0/1 chars in the string
-    for bit in binary_string:
-        is_zero = bit == '0'
-
-        # if the bit value has flipped
-        if bool(was_zero) != bool(is_zero):
-            # the first sequence doesn't count eg: 1111001 has a result of 2
-            if max_count is None:
-                max_count = 0
-            # save the biggest gap
-            elif this_count > max_count:
-                max_count = this_count
-            # reset the counter
-            this_count = 1
+    gap = max_gap = 0
+    for char in binary_string:
+        if char == "0":
+            gap += 1
         else:
-            # increment the length of the sequence
-            this_count += 1
+            max_gap = max(gap, max_gap)
+            gap = 0
 
-        # track what the last bit was
-        was_zero = is_zero
-
-    #print "%s: %s = %s" % (N, binary_string, max_count)
-    if max_count is not None:
-        return max_count
-    else:
-        # no binary gaps found
-        return 0
-
+    return max_gap
 
 
 class TestBinaryGap(unittest.TestCase):
+    MAXINT = 2147483647  # The largest input we need worry about.
+
+    def test_examples(self):
+        self.assertEqual(solution(9), 2)
+        self.assertEqual(solution(529), 4)
+        self.assertEqual(solution(20), 1)
+        self.assertEqual(solution(15), 0)
+        self.assertEqual(solution(32), 0)
 
     def test_example1(self):
         self.assertEqual(5, solution(1041))
@@ -69,7 +77,7 @@ class TestBinaryGap(unittest.TestCase):
     def test_extremes(self):
         self.assertEqual(0, solution(1))
         self.assertEqual(1, solution(5))
-        self.assertEqual(0, solution(MAXINT))
+        self.assertEqual(0, solution(self.MAXINT))
 
     def test_trailing_zeros(self):
         self.assertEqual(solution(6), 0)
@@ -78,6 +86,7 @@ class TestBinaryGap(unittest.TestCase):
     def test_simple1(self):
         self.assertEqual(solution(9), 2)
         self.assertEqual(solution(11), 1)
+        self.assertEqual(solution(32), 0)
 
     def test_simple2(self):
         self.assertEqual(solution(19), 2)
@@ -116,16 +125,6 @@ class TestBinaryGap(unittest.TestCase):
     def test_large6(self):
         self.assertEqual(solution(1610612737), 28)
 
-    def test_non_int(self):
-        self.assertRaises(TypeError, solution, 1.0)
 
-    def test_zero(self):
-        self.assertRaises(ValueError, solution, 0)
-
-    def test_maxint_plus_one(self):
-        self.assertRaises(ValueError, solution, 2147483648)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-    solution(1)

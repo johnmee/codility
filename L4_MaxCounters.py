a28905a82fdc034d6c33c354998e51e5a6e06e4f
John Mee <git.commit@johnmee.com>
2022-08-25 09:21:15 +1000

    Update L4_MaxCounters.

--- L4_MaxCounters.py
+++ L4_MaxCounters.py
@@ -1,24 +1,19 @@
 """
-MaxCounters
+# MaxCounters
 
-Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all
-counters to current maximum
+Calculate the values of counters after applying all alternating operations:
+increase counter by 1; set value of all counters to current maximum.
 
-*********
-This one was tricksy.  The straight-forward solution, even though quite fast and elegant, involves updating all the
-counters every time you get an update-all instruction.  This is the difference between a 60% score and a 100% score.
+You are given N counters, initially set to 0, and you have two possible
+operations on them:
 
-To avoid this hit you have to track the low and high water marks and refer to them as you update individual counters.
-Then, before sending back the results, add a pass over the counters to ensure they are all up to the low-water mark.
-*********
+    * increase(X) - counter X is increased by 1,
+    * max counter - all counters are set to the maximum value of any counter.
 
-You are given N counters, initially set to 0, and you have two possible operations on them:
+A non-empty array A of M integers is given.
+This array represents consecutive operations:
 
-    * increase(X) - counter X is increased by 1
-    * max counter - all counters are set to the maximum value of any counter
-
-A non-empty zero-indexed array A of M integers is given. This array represents consecutive operations:
-    * if A[K] = X, such that 1 <= X <=  N, then operation K is increase(X)
+    * if A[K] = X, such that 1 <= X <=  N, then operation K is increase(X),
     * if A[K] = N + 1 then operation K is max counter.
 
 For example, given integer N = 5 and array A such that:
@@ -45,15 +40,10 @@ Write a function:
 
     def solution(N, A)
 
-that, given an integer N and a non-empty zero-indexed array A consisting of M integers, returns a sequence of
-integers representing the values of the counters.
+that, given an integer N and a non-empty array A consisting of M integers,
+returns a sequence of integers representing the values of the counters.
 
-The sequence should be returned as:
-
-        a structure Results (in C), or
-        a vector of integers (in C++), or
-        a record Results (in Pascal), or
-        an array of integers (in any other programming language).
+Result array should be returned as an array of integers.
 
 For example, given:
     A[0] = 3
@@ -66,61 +56,88 @@ For example, given:
 
 the function should return [3, 2, 2, 4, 2], as explained above.
 
-Assume that:
+Write an efficient algorithm for the following assumptions:
 
         N and M are integers within the range [1..100,000];
         each element of array A is an integer within the range [1..N + 1].
 
-Complexity:
+Copyright 2009–2022 by Codility Limited. All Rights Reserved.
+Unauthorized copying, publication or disclosure prohibited.
+---
 
-        expected worst-case time complexity is O(N+M);
-        expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for
-        input arguments).
+# My Notes
 
-Elements of input arrays can be modified.
+The straight-forward solution, of updating all the counters every time
+there is a 'max counter' instruction, only gets you a 60% score. If every
+instruction is a max-counter it will pass over the whole counter array M
+times! O(n*m)
+
+For 100%, optimize the max-counter operation by, to use a tidal analogy,
+tracking the "low" and "high" "water" marks across all the counters.
+
+With each counter update, check the low-water mark and, if necessary,
+raise this counter to it. Now increment. And, before you leave, if necessary,
+reset the high water mark.  This way, you maintain context of what is happening
+in all the counters, without needing to visit them repeatedly.
+
+Before sending back the results, a final pass over the counters is necessary to
+ensure the low-water mark applies to any counters which have not been in focus
+since it last changed.
+
+Thus the solution requires just two passes over the whole array of counters. O(n+m)
 """
-
 import unittest
 import random
-
-MAX_INT = 100000
-INT_RANGE = (1, MAX_INT)
 
 
 def solution(N, A):
     """
-    :param N: integer - the number of counters
-    :param A: a sequence of integers specifying which counter to increase
-    :return: the list of counters' values
+    :param N: [int] The number of counters.
+    :param A: list[int] A sequence of instructions.
+    :return: list[int] The list of final counter values.
+
+    Create the list of counters and two marker values for max high and min low.
+    For every operation
+      if a "raise counter" operation, then
+          first, raise that counter to the "low water" mark
+          next, increment the counter
+          finally, update the "high water", if necessary
+      if a "max counter" operation then
+          raise the "low water" mark to the "high water" mark
+    For every counter
+      raise any counter below the "low water" mark to the "low water" mark.
     """
-    counters = [0] * (N + 1)   # make it one longer so the counter number works as an index without being off by one
-    maximum = 0
-    minimum = 0
 
-    # step through the array of instructions
-    for key in A:
-        if key <= N:
-            # align with the minimum
-            if counters[key] < minimum:
-                counters[key] = minimum
-            # now increment it
-            counters[key] += 1
-            # update the maximum
-            if counters[key] > maximum:
-                maximum = counters[key]
-        else:
-            # an update-all instruction resets the minimum
-            minimum = maximum
+    counters = [0] * (N + 1)
+    high_water = low_water = 0
 
-    # set any counters below the minimum to the minimum
-    for key, value in enumerate(counters):
-        if value < minimum:
-            counters[key] = minimum
+    for index in A:
+        if index > N:   # 'Max-Counter' operation
+            low_water = high_water
+        else:          # 'Increase-Counter' operation
+            counters[index] = max(counters[index], low_water)
+            counters[index] += 1
+            high_water = max(counters[index], high_water)
 
-    return counters[1:]  # trim the zero array position off
+    for index, value in enumerate(counters):
+        counters[index] = max(low_water, value)
+
+    return counters[1:]  # Trim away the extraneous counter for zero values.
 
 
 class TestExercise(unittest.TestCase):
+    """
+    example: given
+    single: only one counter
+    small_random1: small random test 6 max_counter operations
+    small_random2: small random test, 10 max_counter operations
+    medium_random1: medium random test, 50 max_counter operations
+    medium_random2: medium random test, 500 max_counter operations
+    large_random1: large random test, 2120 max_counter operations
+    large_random2: large random test, 10000 max_counter operations
+    extreme_small: all max_counter operations
+    extreme_large: all max_counter operations
+    """
 
     def test_example(self):
         self.assertEqual(solution(5, [3, 4, 4, 6, 1, 4, 4]), [3, 2, 2, 4, 2])
@@ -128,7 +145,7 @@ class TestExercise(unittest.TestCase):
     def test_singles(self):
         self.assertEqual(solution(1, [1]), [1])
         self.assertEqual(solution(1, [1, 2]), [1])
-        self.assertNotEqual(solution(1, [1,2,2]), [2])
+        self.assertNotEqual(solution(1, [1, 2, 2]), [2])
         self.assertEqual(solution(1, [1, 1, 2, 1, 1]), [4])
 
     def test_doubles(self):
@@ -141,22 +158,10 @@ class TestExercise(unittest.TestCase):
         self.assertEqual(solution(2, [1, 1, 3, 2, 2]), [2, 4])
 
     def test_extreme(self):
-        # ten thousand counters, 90 thousand operations
-        arr = [random.randint(1, 9999) for _ in xrange(90000)]
-        print solution(9999, arr)
+        # Ten thousand counters, 90 thousand operations.
+        arr = [random.randint(1, 9999) for _ in range(90000)]
+        print(solution(9999, arr))
 
-"""
-example
-extreme_small: all max_counter operations
-single: only one counter
-small_random1: small random test 6 max_counter operations
-small_random2: small random test, 10 max_counter operations
-medium_random1: medium random test, 50 max_counter operations
-medium_random2: medium random test, 500 max_counter operations
-large_random1: large random test, 2120 max_counter operations
-large_random2: large random test, 10000 max_counter operations
-extreme_large: all max_counter operations
-"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

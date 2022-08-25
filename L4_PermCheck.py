120987f53de5acdbbf2aada5e2249b7c4cabc73a
John Mee <git.commit@johnmee.com>
2022-08-25 10:41:08 +1000

    Update L4_PermCheck.

--- L4_PermCheck.py
+++ L4_PermCheck.py
@@ -1,9 +1,8 @@
 """
-PermCheck
-
+# PermCheck
 Check whether array A is a permutation.
 
-A non-empty zero-indexed array A consisting of N integers is given.
+A non-empty array A consisting of N integers is given.
 
 A permutation is a sequence containing each element from 1 to N once, and only once.
 
@@ -24,9 +23,9 @@ The goal is to check whether array A is a permutation.
 
 Write a function:
 
-    int solution(int A[], int N);
+    def solution(A)
 
-that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
 
 For example, given array A such that:
     A[0] = 4
@@ -43,56 +42,58 @@ Given array A such that:
 
 the function should return 0.
 
-Assume that:
+Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [1..100,000];
         each element of array A is an integer within the range [1..1,000,000,000].
 
-Complexity:
-
-        expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
-
-Elements of input arrays can be modified.
+Copyright 2009–2022 by Codility Limited. All Rights Reserved.
+Unauthorized copying, publication or disclosure prohibited.
 """
 import unittest
 import random
-
-ARR_RANGE = (1, 100000)
-INT_RANGE = (1, 1000000000)
 
 
 def solution(A):
     """
     :param A: a list of integers
     :return: true if the list is a permutation (sequence from 1 to N)
+
+    One-by-one, add each value to a set.
+        Abort if the value is not valid for the permutation
+        Abort if the value is a duplicate
+    It is a permutation.
     """
-    # an empty list is not a permutation
     if not len(A):
-        return 0
+        return 0  # An empty list is not a permutation.
 
-    # track the hits with a dictionary
-    hits = {}
+    collection = set()
+    length = 0
     for item in A:
-        # (quick exit) each element once and only once thanks
-        if item in hits:
-            return 0
-        hits[item] = True
-
-    # (quick exit again) each element once and only once
-    if len(hits) != len(A):
-        return 0
-
-    # ok, see if they're all there
-    for num in range(1, len(A) + 1):
-        if num not in hits:
-            return 0
-
+        if item > len(A):
+            return 0  # Item is too big for the permutation.
+        collection.add(item)
+        length += 1
+        if len(collection) != length:
+            return 0  # Length of set did not increase, so item is a duplicate.
     return 1
 
 
 class TestExercise(unittest.TestCase):
-
+    """
+    example1
+    example2
+    single element
+    double = two elements
+    antiSum1 = total sum is correct but it is not a permutation, N <= 10
+    small_permutation = permutation + one element occurs twice, N = ~100
+    medium_permutation = permutation + few elements occur twice, N = ~10000
+    antisum2 = total sum is correct but it is not a permutation, N = ~100000
+    large_permutation: permutation + one element occurs twice, N = ~100000
+    large_range: sequence 1,2,...,N  N=~100000
+    extreme_min_max = single element with min/max value
+    extreme_values: all the same values, N=~100000
+    """
     def test_example1(self):
         self.assertEqual(solution([4, 1, 3, 2]), 1)
         self.assertEqual(solution([4, 1, 3]), 0)
@@ -114,28 +115,16 @@ class TestExercise(unittest.TestCase):
         self.assertEqual(solution([3, 3, 3, 3, 2, 1]), 0)
 
     def test_extreme(self):
+        arr_range = (1, 100000)
+
         # full complement of numbers
-        arr = range(*ARR_RANGE)
+        arr = list(range(*arr_range))
         random.shuffle(arr)
         self.assertEqual(solution(arr), 1)
 
         # full complement with one missing
-        arr.remove(random.randint(1,len(arr)))
+        arr.remove(random.randint(1, len(arr)))
         self.assertEqual(solution(arr), 0)
-
-
-# example1
-# example2
-# extreme_min_max = single element with min/max value
-# single element
-# double = two elements
-# antiSum1 = total sum is correct but it is not a permutation, N <= 10
-# small_permutation = permutation + one element occurs twice, N = ~100
-# medium_permutation = permutation + few elements occur twice, N = ~10000
-# antisum2 = total sum is correct but it is not a permutation, N = ~100000
-# large_permutation: permutation + one element occurs twice, N = ~100000
-# large_range: sequence 1,2,...,N  N=~100000
-# extreme_values: all the same values, N=~100000
 
 
 if __name__ == '__main__':

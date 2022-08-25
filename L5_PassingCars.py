8beae5ec13bce2010b0541fb7af1bca3783135e4
John Mee <git.commit@johnmee.com>
2022-08-25 16:17:59 +1000

    Update L5_PassingCars.

--- L5_PassingCars.py
+++ L5_PassingCars.py
@@ -1,29 +1,19 @@
 """
-Passing Cars
+# Passing Cars
+
 Count the number of passing cars on the road.
 
-
-
-***
-Note:
-
-    The cars travelling east only pass cars travelling west if they appear after it in the series.
-
-The only indication of this feature is the example.  And even when I started to suspect it, there is
-no second example with which to establish whether this observation is a feature or a coincidence.
-
-* you can invert the problem to a count of cars travelling east that pass cars travelling west no problem.
-********
-
-A non-empty zero-indexed array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+A non-empty array A consisting of N integers is given.
+The consecutive elements of array A represent consecutive cars on a road.
 
 Array A contains only 0s and/or 1s:
 
         0 represents a car traveling east,
         1 represents a car traveling west.
 
-The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 <= P < Q < N, is passing when P is
-traveling to the east and Q is traveling to the west.
+The goal is to count passing cars. We say that a pair of cars (P, Q),
+where 0 ≤ P < Q < N, is passing when P is traveling to the east
+and Q is traveling to the west.
 
 For example, consider array A such that:
   A[0] = 0
@@ -36,9 +26,9 @@ We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
 
 Write a function:
 
-    int solution(int A[], int N);
+    def solution(A)
 
-that, given a non-empty zero-indexed array A of N integers, returns the number of pairs of passing cars.
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
 
 The function should return -1 if the number of pairs of passing cars exceeds 1,000,000,000.
 
@@ -51,44 +41,80 @@ For example, given:
 
 the function should return 5, as explained above.
 
-Assume that:
+Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [1..100,000];
         each element of array A is an integer that can have one of the following values: 0, 1.
 
-Complexity:
-
-        expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
-
-Elements of input arrays can be modified.
+Copyright 2009–2022 by Codility Limited. All Rights Reserved.
+Unauthorized copying, publication or disclosure prohibited.
 """
 import unittest
 import random
 
 
-MAX_INT = int(100000)
 MAX_PAIRS = int(1e9)
 
 
 def solution(A):
+    """Count the number of passing cars on the road.
+
+    :param A: list[int] A sequence of cars traveling east (0) or west (1).
+    :return: [int] A count of the pairs of cars passing, or -1.
+
+    NB: A car travelling east only 'passes' the cars travelling west, which are
+    after it in the series.
+
+    Another way to think of it is:
+    "For every 0, how many 1's does it pass through to get to the right end
+    of the array?"
+
+    Solution A: O(log(N))
+
+    From the left, we grab every "0" and move it right, counting the
+    number of "1"s it passes, then sum all those counts together.
+    This passes every element in the array over every other element
+    to its right in the array.
+
+    Solution B: O(N)
+
+    From the left, we count every '0'. When we encounter a '1' we add the
+    current count, all the cars going east that will pass this one going west,
+    to the total: O(N).
+
+    Lesson:
+    When passing over a dataset think about how accumulating metadata about that
+    data can reduce the need to see the data again.
+
+    In this example, we accumulate the count of cars going east so that, when
+    we encounter a car going west, we can apply the metadata and never look back.
     """
-    :param A: array of int (0 or 1 which are actually booleans)
-    :return: integer count of pairs of passing cars, or -1 if more than 1e9 pairs
-    """
-    east = 0
-    pairs = 0
-    for car in A:
-        if not bool(car):
+    east = passes = 0
+    for direction in A:
+        if direction == 0:  # Going East.
             east += 1
-        else:
-            pairs += east
-        if pairs > MAX_PAIRS:
+        else:  # Going West.
+            passes += east
+        if passes > MAX_PAIRS:  # They do test for this!
             return -1
-    return pairs
+    return passes
 
 
 class TestExercise(unittest.TestCase):
+    """
+    example: example test
+    single: single element
+    double: two elements
+    simple: simple test
+    small_random: random, length = 100
+    small_random2: random, length = 1000
+    medium_random: random, length = ~10,000
+    large_random: random, length = ~100,000
+    large_big_answer: 0..01..1, length = ~100,000
+    large_alternate: 0101..01, length = ~100,000
+    large_extreme: large test with all 1s/0s, length = ~100,000
+    """
+
     def test_example(self):
         self.assertEqual(solution([0, 1, 0, 1, 1]), 5)
 
@@ -127,8 +153,8 @@ class TestExercise(unittest.TestCase):
         self.assertEqual(solution([1, 1, 1, 1]), 0)
 
     def test_extreme(self):
-        self.assertEqual(solution([random.randint(0,1) for _ in xrange(int(1e6))]), -1)
+        self.assertEqual(-1, solution([random.randint(0, 1) for _ in range(int(1e6))]))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
